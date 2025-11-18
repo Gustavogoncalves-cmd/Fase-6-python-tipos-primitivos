e7520b11747c859a766d0533fb5b594230d77747
Gustavogoncalves-cmd <Aleatorizando57@gmail.com>
2025-11-18 16:43:10 -0300

    Atualizando exercícios da aula 1

---
+++ exercicio 1.1.py
@@ -0,0 +1,5 @@
+n1=int(input('digite um numero'))
+n2=int(input('digite outro'))
+s=n1+n2 #n1= 0, n2=2 e s=2 Se for colocar entre as chaves
+#print('a soma entre', n1, 'e', n2, 'vale', s)
+print('a soma entre {0} e {1} igual a  {2}' .format(n1, n2, s)) #entre as chaves somente numeros são validos!
