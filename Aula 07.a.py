216d05f111a0b3c5b3445541ea58270acc8703e0
Gustavogoncalves-cmd <Aleatorizando57@gmail.com>
2025-11-19 17:07:21 -0300

    Aula 07.a operadores aritmeticos

---
+++ Aula 07.a.py
@@ -0,0 +1,10 @@
+n1=int(input('Digite um valor! '))
+n2=int(input('outro valor: '))
+s= n1+n2
+m= n1*n2
+d= n1/n2
+di= n1//n2
+e= n1**n2
+
+print(f'a soma e {s}, o produto e {m}, e a divisao e {d:.3f}', end=' ')
+print(f'divisao inteira {di}, e potencia {e}')
