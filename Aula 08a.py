fbe0d8b335e935c54c7999bfb1c7dfaf73915a80
Gustavogoncalves-cmd <Aleatorizando57@gmail.com>
2025-12-12 20:47:02 -0300

    aula 08a.py

--- Aula 08a.py
+++ Aula 08a.py
@@ -1,6 +1,6 @@
-import math
+from math import sqrt
 
 num = int(input('Digite um numero: '))
-raiz = math.sqrt(num)
+raiz = sqrt(num)
 
-print(f'a raiz de {num} é igual a {math.floor(raiz)}')
+print(f'a raiz de {num} é igual a {(raiz)}') 
---
+++ ex016.py
@@ -0,0 +1,3 @@
+n1 = float(input('digite um valo! '))
+num = int(n1)
+print (f'o valor digitado foi {n1} e sua porcao inteira e {num}')
