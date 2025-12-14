5dd23d42c9078e87977293873c6f06e1112c03a1
Gustavogoncalves-cmd <Aleatorizando57@gmail.com>
2025-12-14 09:43:30 -0300

    Create ex017.py

---
+++ ex017.py
@@ -0,0 +1,24 @@
+co = float(input('comprimento do cateto oposto: '))
+ca = float(input('comprimento do cateto adjacente: '))
+hi= (co ** 2 + ca ** 2) ** (1/2)
+
+print (f'a hipotenusa vai medir {hi:.2f}')
+
+
+
+
+
+
+
+
+
+
+
+#from math import hypot
+#co = float(input('qual a medida do cateto oposto? '))
+#ca = float(input('qual a medida do cateto adjacente? '))
+#hi = hypot(co, ca)
+
+#print (f'a impotenusa vai medir {hi:.2f} ')
+
+
