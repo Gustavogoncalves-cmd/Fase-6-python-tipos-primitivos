d9fb8eb274ef3225b1b1ab6b6149e8a52a2e887f
Gustavogoncalves-cmd <Aleatorizando57@gmail.com>
2025-12-17 16:35:49 -0300

    conclusao da aula 09 do curso em video mundo 1

---
+++ aula09.py
@@ -0,0 +1,33 @@
+#frase = 'curso em video python'
+#print(frase) escreve o objeto dentro de frase
+#print (frase[3]) estamos fatiando nossa string
+#print (frase[3:13]) estamos fatiando da 4 letra ate a 12
+#print (frase[13:]) se colocarmos um numero antes de ":" vamos indicar o incio mas nao o final do fatiamento e visse e versa
+#print (frase[1:15:2]) se adicionarmos mais Um sinal de ":" nos vamos comecas a pular de 2 em dois o fatiamento
+#print (frase[1::2]) se retirarmos o numero do meio nos deixamos de definir o final
+#print (frase[::2]) se  retirarmos a primeira vamos deixar de definir o inicio e ele vai continuar a pular de 2 em 2 do inicio ao fim
+
+#print longo: 
+# print ('''ATENÇÃO: O botão de concluir aula só fica clicável depois que toda a aula é assistida
+# Para avançar no andamento dos cursos você precisa sempre:
+# 1) Ver o vídeo até o final
+# 2) Clicar no botão de "CLIQUE AQUI PARA MARCAR COMO CONCLUÍDO" das aulas, capitulos, módulos e cursos após ver o vídeo ou antes de avançar para aula seguinte.
+# Assistindo ao vídeo até o final e clicando no botão de concluído ele deve conseguir avançar sem problemas.''')
+
+frase = 'curso em video python'
+#print(frase.count('o')) vai contar quantos "o" minusculos tem na frase
+#print(frase.upper().count('O'))
+#print(len(frase)) o comando(len) contas quantas lentras tem na frase. Lembrando: espacos contam!
+#print(len(frase.stripe())) se adicionarmos o comando .stripe ele remove os espacos indesejados como os do incio e do final caso exista!
+#frase = (frase.replace('Python', 'Android')) ele da a possibilidade de subistituir a frase
+#print(frase)
+
+#print('curso' in frase) verifica se a palavra entre '' esta dentro de frase.
+#print(frase.find('curso')) verifica a posicao
+#print(frase.find('VIDEO')) se nao existir essa palavra ou se ela existir de forma diferente da que esta escrita ele retorna como -1 pois 1- = inexistente
+#print(frase.lower().find('video')) ele vai fazer uma busca por uma frase jogada pra minuscula
+#print(fase.split()) o splite vai dividir as palavras da frase
+#dividido = frase.split() ---- objeto
+#print(dividido) ele vai divir cada palavra da frase
+#print(dividido[0]) ele vai mostrar a plavra na posicao 0
+#print(dividido[2][2]) ele vai pegar a palvra na posicao 2 e vai mostrar a letra dentro da palvra na posicao 2
